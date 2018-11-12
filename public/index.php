cf4dff66a97cf30747f005d145da5a894d8cda6f
Seif <bromdhane@gmail.com>
2018-11-12 04:11:36 +0100

    Update public/index.php

--- public/index.php
+++ public/index.php
@@ -1,9 +1,7 @@
 <?php
 /**
- * User Seif <ben.s@mipih.fr>
- * Date time: 06/11/2018 19:08
+ * @author Seif
  */
-
 require_once __DIR__.'/../autoload.php';
 $ca = 100000;
 $type = \Factory\EntrepriseFactory::ENTREPRISE_TYPE_SAS;
